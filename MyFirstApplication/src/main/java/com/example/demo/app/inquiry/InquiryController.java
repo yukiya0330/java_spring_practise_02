4366fa092c9e7429e46e33f14d861b8951620287
yukiya <hosiyky33077@gmail.com>
2022-09-23 11:36:35 +0900

    inquiry_application_finish

--- MyFirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ MyFirstApplication/src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -2,7 +2,10 @@ package com.example.demo.app.inquiry;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,9 +13,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InquiryController {
 
 	@GetMapping("/form")
-	public String form(Model model) {
+	public String form(InquiryForm inquiryForm, Model model) {
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/form";
+	}
+
+	@PostMapping("/form")
+	public String formGoBack(InquiryForm inquiryForm, Model model) {
+		model.addAttribute("title", "Inquiry Form");
+		return "inquiry/form";
+	}
+
+
+	@PostMapping("/confirm")
+	public String confirm(@Validated InquiryForm inquiryForm,
+			BindingResult result,
+			Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("title", "inquiry Form");
+		return "inquiry/form";
+		}
+		model.addAttribute("title", "confirm Page");
+		return "inquiry/confirm";
 	}
 
 }
