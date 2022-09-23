1ae320be25dcb2f3c337d2fdfcb5f1182a698ebe
yukiya <hosiyky33077@gmail.com>
2022-09-23 20:41:03 +0900

    survey_application

---
+++ MyFirstApplication/src/main/java/com/example/demo/app/survey/SurveyForm.java
@@ -0,0 +1,44 @@
+package com.example.demo.app.survey;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class SurveyForm {
+
+	@Min(0)
+	@Max(150)
+	private int age;
+
+	@Min(1)
+	@Max(5)
+	private int satisfaction;
+
+	@NotNull
+	@Size(min = 1, max = 200, message = "Please 1-200")
+	private String comment;
+
+	public SurveyForm() {
+	}
+
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSatisfaction() {
+		return satisfaction;
+	}
+	public void setSatisfaction(int satisfaction) {
+		this.satisfaction = satisfaction;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+}
--- MyFirstApplication/src/main/resources/schema.sql
+++ MyFirstApplication/src/main/resources/schema.sql
@@ -7,3 +7,13 @@ CREATE TABLE inquiry
    created DATETIME NOT NULL,
    PRIMARY KEY(id)
 );
+
+CREATE TABLE survey
+(
+   id INT NOT NULL AUTO_INCREMENT,
+   age INT NOT NULL,
+   satisfaction INT NOT NULL,
+   comment VARCHAR(100),
+   created DATETIME NOT NULL,
+   PRIMARY KEY(id)
+);
